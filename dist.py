405cd772e63c8aa18d888b75dcc82fab5f050e2f
Majd <its.mjdhere@gmail.com>
2026-07-31 17:26:00 +0300

    Updated distance module to support X and Y distances

--- dist.py
+++ dist.py
@@ -2,16 +2,24 @@ import time
 
 def distance():
     try:
-        print("Enter the bigger coordinate: ")
-        d1_input = input(">")
-        d1_input_helper = int(d1_input)
-        print("Enter the smaller coordinate: ")
-        d2_input = input(">")
-        d2_input_helper = int(d2_input)
+        print("Enter the bigger coordinate X: ")
+
+        d1_input_x = input(">")
+        d1_input_helper_x = int(d1_input_x)
+        print("Enter the bigger coordinate Y: ")
+        d1_input_y = input(">")
+        d1_input_helper_y = int(d1_input_y)
+        print("Enter the smaller coordinate X: ")
+        d2_input_x = input(">")
+        d2_input_helper_x = int(d2_input_x)
+        print("Enter the smaller coordinate Y: ")
+        d2_input_y = input(">")
+        d2_input_helper_y = int(d2_input_y)
         print ("calculating..")
         time.sleep(3)
-        result = d1_input_helper - d2_input_helper
-        print("the distance is:", result,"blocks")
+        result_x = d1_input_helper_x - d2_input_helper_x
+        result_y = d1_input_helper_y - d2_input_helper_y
+        print("the distance is:", result_x, "blocks in X and", result_y, "blocks in Y")
         time.sleep(2)
         return
     except ValueError:
